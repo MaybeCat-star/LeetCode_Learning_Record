775a63cc10fcc1b79176b913114cb644b0492369
Will Lee <40391650+MaybeTommorow@users.noreply.github.com>
2021-12-01 17:29:46 +0800

    Update LCSolution.py

--- LeetCodeSolution_python/LCSolution.py
+++ LeetCodeSolution_python/LCSolution.py
@@ -232,3 +232,16 @@ class Solution:
         nums[:] = nums[n - k:] + nums[:n - k]
 
 
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = [[0] * 9 for _ in range(9)]
+        col = [[0] * 9 for _ in range(9)] 
+        box = [[0] * 9 for _ in range(9)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                curNum = ord(board[i][j]) - ord('1')
+                if row[i][curNum] != 0 or col[j][curNum] != 0 or box[j // 3 + (i // 3) * 3][curNum] != 0:
+                    return False
+                row[i][curNum], col[j][curNum], box[j // 3 + (i // 3) * 3][curNum] = 1, 1, 1
+        return True

d62c1186cb42cabf302d1ad2026e0bae3c3a9ca5
Will Lee <40391650+MaybeTommorow@users.noreply.github.com>
2021-12-02 08:32:49 +0800

    增加整数反转

--- LeetCodeSolution_python/LCSolution.py
+++ LeetCodeSolution_python/LCSolution.py
@@ -245,3 +245,45 @@ class Solution:
                     return False
                 row[i][curNum], col[j][curNum], box[j // 3 + (i // 3) * 3][curNum] = 1, 1, 1
         return True
+
+
+    # 翻转整数，我的代码：
+    def reverse(self, x: int) -> int:
+        ans = 0
+        tmp = x
+        count = 0
+        n = 1
+        if x < 0:
+            n *= -1
+            x *= -1
+            tmp *= -1
+        while tmp // 10 != 0:
+            count += 1
+            tmp //= 10
+        while count >= 0:
+            ans += (x % 10) * (10 ** count)
+            x //= 10
+            count -= 1
+        if ans > 2 ** 31 - 1 or ans < 2 ** -31:
+            return 0
+        return ans * n
+    # 官方题解：
+    # def reverse(self, x: int) -> int:
+    #     INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
+    #     rev = 0
+    #     while x != 0:
+    #         # INT_MIN 也是一个负数，不能写成 rev < INT_MIN // 10
+    #         if rev < INT_MIN // 10 + 1 or rev > INT_MAX // 10:
+    #             return 0
+    #         digit = x % 10
+    #         # Python3 的取模运算在 x 为负数时也会返回 [0, 9) 以内的结果，因此这里需要进行特殊判断
+    #         if x < 0 and digit > 0:
+    #             digit -= 10
+
+    #         # 同理，Python3 的整数除法在 x 为负数时会向下（更小的负数）取整，因此不能写成 x //= 10
+    #         x = (x - digit) // 10
+    #         rev = rev * 10 + digit
+        
+    #     return rev   
+
